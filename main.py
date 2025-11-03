4611c44396222350b5136775aa149d756769ebc2
Fal <129197356+Helvardt@users.noreply.github.com>
2025-11-03 22:28:16 +0100

    book add to main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath, encoding='utf-8') as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    book = "books/frankenstein.txt"
+    return get_book_text(book)
+    
+main()
